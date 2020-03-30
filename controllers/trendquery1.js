7166797c0d6cfe57950eee142acf622e78bd0017
Derek <39328219+dpadilla1@users.noreply.github.com>
2020-03-29 19:08:58 -0700

    Trendquery1 is fully parameterized.

--- controllers/trendquery1.js
+++ controllers/trendquery1.js
@@ -4,6 +4,8 @@ const database = require('../services/database.js');
 async function get(req, res, next) {
   try {
     console.log("REQUEST PARAMS: ", req.query);
+    //---TEST ATTEMPT---//
+    /*
     let sY = req.query.startYear;
     let eY = req.query.endYear;
     const baseQuery = 
@@ -14,7 +16,30 @@ async function get(req, res, next) {
         AND seasonYear BETWEEN ` + sY + ` AND ` + eY + 
     ` GROUP BY seasonYear, playType
     HAVING playType='RUSH' OR playType='PASS'
-    ORDER BY playType ASC, seasonyear`;
+    ORDER BY playType ASC, seasonyear`;*/
+
+    let pT = req.query.playType;
+    let sY = req.query.seasonYear;
+    let teamList = req.query.teamList;
+    let tL = "";
+    for(i=0; i<teamList.length; i++) {
+        tL += "'" + teamList[i] + "',";
+    }
+    tL += "'end'";
+
+    const baseQuery = 
+    `SELECT SUBSTR(da,4,6) as MONTH, seasonYear, offenseteam, playtype, avgYards
+     FROM (
+        SELECT seasonYear, TRUNC(gamedate, 'MONTH') da, offenseteam, playType, AVG(Yards) avgYards
+        FROM NFL_PLAYBYPLAY npbp, gamestats, game
+        WHERE npbp.gamestatsid = gamestats.gamestatsid
+            AND gamestats.gameid = game.gameid
+            AND playType='` + pT + `' 
+            AND seasonYear = ` + sY + ` 
+            AND offenseteam IN (` + tL + `)
+        GROUP BY seasonYear, offenseteam, TRUNC(gamedate, 'MONTH'), playType
+        ORDER BY seasonYear, offenseteam, TRUNC(gamedate, 'MONTH')
+        )`;
  
     const binds = {};
     const result = await database.simpleExecute(baseQuery, binds);
